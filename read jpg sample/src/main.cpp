989220734bad3657d9998921b78cb9f7866b7aaa
RoundBearChoi <roundbeargames@gmail.com>
2020-12-10 15:42:17 +0900

    reading rgb in unsigned char

--- read jpg sample/src/main.cpp
+++ read jpg sample/src/main.cpp
@@ -28,6 +28,8 @@ int main(int argc, char** argv)
 
 	std::cout << std::endl;
 	
+	// read randompic.jpg and print (current path)
+
 	Image image1("randompic.jpg");
 	
 	std::cout << std::endl;
@@ -35,11 +37,45 @@ int main(int argc, char** argv)
 	if (image1.data != nullptr)
 	{
 		std::cout << "image1 read successfully!" << std::endl;
+
+		std::cout << std::endl;
+
 		std::cout << "width: " << image1.w << std::endl;
 		std::cout << "height: " << image1.h << std::endl;
+
+		std::cout << std::endl;
+
+		for (int i = 0; i < image1.w * image1.channels; i++)
+		{
+			int n = (int)image1.data[i];
+			std::cout << n << " ";
+		}
+
+		std::cout << std::endl;
 	}
 
+	std::cout << std::endl;
+
+	// read randompic2 at a specific location
+
 	Image image2("RandomFolder/randompic2.jpg");
+
+	// copy house.jpg to "copied house.jpg"
+
+	Image house("house.jpg");
+	Image copiedhouse = house;
+	copiedhouse.write("copied house.jpg");
+
+	// create white.jpg
+
+	Image white(100, 100, 3);
+	
+	for (int i = 0; i < white.w * white.h * white.channels; i++)
+	{
+		white.data[i] = 255;
+	}
+
+	white.write("white.jpg");
 
 	return 0;
 }
